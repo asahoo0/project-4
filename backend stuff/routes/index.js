b6aeaf3da8eadd970de35bb97b7b39c949af625e
asahoo0 <73152261+asahoo0@users.noreply.github.com>
2024-12-15 16:52:58 -0600

    Delete backend stuff directory

--- backend stuff/routes/index.js
+++
@@ -1,7 +0,0 @@
-var teams = require("./teams");
-var leagues = require("./leagues");
-// var users = require("./users");
-module.exports = function (app, router) {
-    app.use('/teams', teams);
-    app.use('/leagues', leagues);
-};
