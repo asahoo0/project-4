3ae998aee3c4be47cde38d80c00a5a8f95566477
asahoo0 <73152261+asahoo0@users.noreply.github.com>
2024-12-15 16:53:22 -0600

    Delete my-project directory

--- my-project/backend/database.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-const connection = "mongodb+srv://test:1234@cluster0.d2ur5pe.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
-    .then(() => console.log("Database Connected Successfully"))
-    .catch(err => console.log(err));
