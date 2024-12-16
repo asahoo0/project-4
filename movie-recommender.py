c85a2d23346d038aa30fb20ae92e0e86f5b7a400
asahoo0 <73152261+asahoo0@users.noreply.github.com>
2024-12-15 19:03:33 -0600

    Update movie-recommender.py

--- movie-recommender.py
+++ movie-recommender.py
@@ -9,125 +9,124 @@ st.set_page_config(layout="wide")
 
 @st.cache_data
 def load_data():
-    base_folder = "data"
-    ratings = pd.read_csv(f'{base_folder}/ratings.dat', sep='::', engine = 'python', header=None)
-    ratings.columns = ['UserID', 'MovieID', 'Rating', 'Timestamp']
+    data_folder = "data"
+    user_ratings = pd.read_csv(f'{data_folder}/ratings.dat', sep='::', engine='python', header=None)
+    user_ratings.columns = ['UserID', 'MovieID', 'Rating', 'Timestamp']
 
-    movies = pd.read_csv(f'{base_folder}/movies.dat', sep='::', engine = 'python',
-                         encoding="ISO-8859-1", header = None)
-    movies.columns = ['MovieID', 'Title', 'Genres']
+    film_data = pd.read_csv(f'{data_folder}/movies.dat', sep='::', engine='python',
+                            encoding="ISO-8859-1", header=None)
+    film_data.columns = ['MovieID', 'Title', 'Genres']
 
-    users = pd.read_csv(f'{base_folder}/users.dat', sep='::', engine = 'python', header = None)
-    users.columns = ['UserID', 'Gender', 'Age', 'Occupation', 'Zipcode']
+    demographic_data = pd.read_csv(f'{data_folder}/users.dat', sep='::', engine='python', header=None)
+    demographic_data.columns = ['UserID', 'Gender', 'Age', 'Occupation', 'Zipcode']
 
-    rating_matrix = pd.read_csv(f'{base_folder}/Rmat.csv', sep=',')
-    return (ratings, movies, users, rating_matrix)
-
-@st.cache_data
-def get_random_movie_set(n=10):
-    movie_set = movies.sample(n)
-    return movie_set
+    rating_matrix = pd.read_csv(f'{data_folder}/Rmat.csv', sep=',')
+    return (user_ratings, film_data, demographic_data, rating_matrix)
 
 @st.cache_data
-def build_similarity_matrix_v2():
-    normalized_rating_matrix = rating_matrix.subtract(rating_matrix.mean(axis=1), axis='rows')
+def get_random_movie_sample(n=10):
+    sample_set = film_data.sample(n)
+    return sample_set
 
-    cardinality_df = (~normalized_rating_matrix.isna()).astype('int')
-    cardinality_df = cardinality_df.T
-    cardinality_matrix = cardinality_df @ cardinality_df.T
+@st.cache_data
+def create_similarity_matrix():
+    normalized_ratings = rating_matrix.subtract(rating_matrix.mean(axis=1), axis='rows')
 
-    normalized_rating_matrix = normalized_rating_matrix.T
-    normalized_rating_matrix = normalized_rating_matrix.fillna(0)
+    presence_df = (~normalized_ratings.isna()).astype('int')
+    presence_df = presence_df.T
+    presence_matrix = presence_df @ presence_df.T
 
-    nr = normalized_rating_matrix @ normalized_rating_matrix.T
-    squared_normalized_rating_matrix = ((normalized_rating_matrix**2) @ (normalized_rating_matrix != 0).T)
-    squared_normalized_rating_matrix = squared_normalized_rating_matrix.apply(np.vectorize(np.sqrt))
-    dr = squared_normalized_rating_matrix * squared_normalized_rating_matrix.T
+    normalized_ratings = normalized_ratings.T
+    normalized_ratings = normalized_ratings.fillna(0)
+
+    rating_dot_product = normalized_ratings @ normalized_ratings.T
+    squared_rating_matrix = ((normalized_ratings**2) @ (normalized_ratings != 0).T)
+    squared_rating_matrix = squared_rating_matrix.apply(np.vectorize(np.sqrt))
+    rating_norm = squared_rating_matrix * squared_rating_matrix.T
     
-    cosine_distance = nr / dr
-    S = (1 + cosine_distance) / 2
+    cosine_similarity = rating_dot_product / rating_norm
+    similarity_matrix = (1 + cosine_similarity) / 2
     
-    np.fill_diagonal(S.values, np.nan)
-    S[cardinality_matrix < 3] = None
+    np.fill_diagonal(similarity_matrix.values, np.nan)
+    similarity_matrix[presence_matrix < 3] = None
     
-    return S
+    return similarity_matrix
 
 def myIBCF(S, w, n=10):
     S = S.copy()
     S = S.fillna(0)
 
     w = w.copy()
-    identity = (~w.isna()).astype(int)
+    identity_matrix = (~w.isna()).astype(int)
     w = w.fillna(0)
 
-    reco_movies = w.dot(S) / identity.dot(S)
-    reco_movies = reco_movies.sort_values(ascending=False)[0:n]
+    recommendations = w.dot(S) / identity_matrix.dot(S)
+    recommendations = recommendations.sort_values(ascending=False)[0:n]
     
-    reco_movies = reco_movies.dropna()
+    recommendations = recommendations.dropna()
     
-    return reco_movies
+    return recommendations
 
-def get_recommendations():
-    movie_set["star"] = np.array(star_list)
+def generate_movie_recommendations():
+    sample_set["star"] = np.array(star_ratings)
 
-    row = S.iloc[0]
-    user_ratings = row.copy()
+    user_ratings = similarity_matrix.iloc[0]
     user_ratings[:] = np.nan
 
-    for i in range(movie_set.shape[0]):
-        key = "m" + str(movie_set.iloc[i]["MovieID"])
-        value = movie_set.iloc[i]["star"]
-        if key in user_ratings:
-            user_ratings.loc[key] = value
+    for i in range(sample_set.shape[0]):
+        movie_key = "m" + str(sample_set.iloc[i]["MovieID"])
+        movie_value = sample_set.iloc[i]["star"]
+        if movie_key in user_ratings:
+            user_ratings.loc[movie_key] = movie_value
 
-    reco_movies = myIBCF(S=S, w=user_ratings, n=reco_size)
-    reco_movies = movies[movies["MovieID"].isin(reco_movies.index.str.slice(1).astype(int))]
-    return reco_movies
+    recommended_movies = myIBCF(S=similarity_matrix, w=user_ratings, n=reco_size)
+    recommended_movies = film_data[film_data["MovieID"].isin(recommended_movies.index.str.slice(1).astype(int))]
+    return recommended_movies
 
-(ratings, movies, users, rating_matrix) = load_data()
-S = build_similarity_matrix_v2()
+(user_ratings, film_data, demographic_data, rating_matrix) = load_data()
+similarity_matrix = create_similarity_matrix()
 
 st.header("Collaborative Filtering Movie Recommender System")
-movie_set_size = st.slider("Movie Set size:", 1, 100, 10)
-reco_size = st.slider("Recommendation Set size:", 1, 100, 10)
+sample_size = st.slider("Movie Sample Size:", 1, 100, 10)
+reco_size = st.slider("Recommendation Size:", 1, 100, 10)
 grid_size = st.slider("Display Grid:", 1, 10, 10, key="ibcf_grid_size")
 
 with st.container(border=True):
     with st.expander("Step 1: Rate as many movies as possible", expanded=True):
         st.info("Step 1: Rate as many movies as possible")
-        movie_set = get_random_movie_set(n=movie_set_size)
+        sample_set = get_random_movie_sample(n=sample_size)
 
-        cols = st.columns(grid_size)
+        columns = st.columns(grid_size)
 
-        star_list = []
-        (row, _) = movie_set.shape
-        for i in range(row):
-            record = movie_set.iloc[i, :]
-            with cols[i % grid_size]:
-                title = record['Title']
-                st.subheader(f"{title}")
+        star_ratings = []
+        (num_movies, _) = sample_set.shape
+        for i in range(num_movies):
+            movie_record = sample_set.iloc[i, :]
+            with columns[i % grid_size]:
+                movie_title = movie_record['Title']
+                st.subheader(f"{movie_title}")
 
-                image_url = f'https://liangfgithub.github.io/MovieImages/{record["MovieID"]}.jpg'
+                image_url = f'https://liangfgithub.github.io/MovieImages/{movie_record["MovieID"]}.jpg'
                 st.image(image_url)
 
-                star = st.slider(f"Rate {title}", 1, 5, 3, key=f"stars_{i}")
-                star_list.append(star)
+                rating = st.slider(f"Rate {movie_title}", 1, 5, 3, key=f"stars_{i}")
+                star_ratings.append(rating)
 
 with st.container(border=True):
     st.info("Step 2: Discover movies you might like")
 
-    reco_movies = get_recommendations()
-    (row, _) = reco_movies.shape
+    recommended_movies = generate_movie_recommendations()
+    (num_reco, _) = recommended_movies.shape
 
-    cols = st.columns(grid_size)
+    columns = st.columns(grid_size)
 
-    for i in range(row):
-        record = reco_movies.iloc[i, :]
-        with cols[i % grid_size]:
-            title = record['Title']
-            st.subheader(f"{title}")
+    for i in range(num_reco):
+        movie_record = recommended_movies.iloc[i, :]
+        with columns[i % grid_size]:
+            movie_title = movie_record['Title']
+            st.subheader(f"{movie_title}")
 
             st.text(f"Rank: {i+1}")
 
-            image_url = f'https://liangfgithub.github.io/MovieImages/{record["MovieID"]}.jpg'
+            image_url = f'https://liangfgithub.github.io/MovieImages/{movie_record["MovieID"]}.jpg'
             st.image(image_url)
